c4f74d145e093541d7d937579840c8dbd552dd26
andrew beatrice <abeatrice@consumertrack.com>
2022-03-27 21:21:30 -0700

    add leads to midden on create

---
+++ app/Listeners/PublishLead.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\LeadSaved;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Http;
+
+class PublishLead
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \App\Events\LeadSaved  $event
+     * @return void
+     */
+    public function handle(LeadSaved $event)
+    {
+        $lead = array_filter($event->lead->toArray(), function($k) {
+            return !in_array($k, ['attribution_key', 'updated_at', 'created_at', 'id']);
+        }, ARRAY_FILTER_USE_KEY);
+        $lead['redirected_at'] = (string) $lead['redirected_at'];
+        $lead = collect($lead)->toJson();
+        $lead = base64_encode($lead);
+
+        $url = "https://buonget459.execute-api.us-east-1.amazonaws.com/live/content/{$event->lead->attribution_key}";
+        $response = Http::put($url, [
+            'enc' => $lead,
+        ]);
+    }
+}
--- app/Models/Lead.php
+++ app/Models/Lead.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Events\LeadSaved;
 
 class Lead extends Model
 {
     use HasFactory;
+
+    protected $dispatchesEvents = [
+        'saved' => LeadSaved::class,
+    ];
+
 }
